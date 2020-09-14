1b98ef1a472d10c298760f366d6f7156d8906b11
timothy.reynolds <timothy.reynolds@dsainc.com>
2020-09-14 11:04:33 -0400

    Error handling added

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+) 
 
 // takes a name parameter whose type is string, and returns a string
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+    // If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("name value empty")
+	}
     // Return a greeting that embeds the name in a message.
     //the := operator is a shortcut for declaring and initializing a variable in one line
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
